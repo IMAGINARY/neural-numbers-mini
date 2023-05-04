1688098474ef3c910e71ef448b307570d6def009
Eric Londaits <eric.londaits@gmail.com>
2023-05-04 17:34:55 -0300

    Switch simple-git for degit in postinstall script.
    degit is better because it downloads the code from git directly without transferring history, creating .git, etc.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
-const simpleGit = require('simple-git');
+const degit = require('degit');
 const packageJson = require('../package.json');
 
-Object.keys(packageJson.vendor).forEach((key) => {
+async function fetchFromGit(repository, path) {
+  return degit(repository, { cache: false, force: true }).clone(path);
+}
+
+Object.keys(packageJson.vendor).forEach(async (key) => {
   const props = packageJson.vendor[key];
   const path = `./vendor/${key}-${props.tag}`;
   if (!fs.existsSync(path)) {
-    const git = simpleGit();
-    git.clone(props.repository, path, ['--depth=1', `--branch=${props.tag}`], (err) => {
-      if (err) {
-        console.log(`Error cloning ${key}`, err);
-      }
-    })
+    await degit(props.repository, { cache: false, force: true }).clone(path);
   }
 });
